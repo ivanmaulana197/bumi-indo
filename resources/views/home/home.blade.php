074a3a5b661577838254a4d9e9b87769038445fb
ivanmaulana197 <ivanmaulana@webmail.umm.ac.id>
2021-07-01 09:15:02 +0700

    Update home.blade.php

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -180,26 +180,26 @@
         </div>
         @foreach($deadlineApproaching as $data)
         <div class="modal fade" id="exampleModal{{$data->id}}" tabindex="-1" aria-labelledby="exampleModalFullscreenLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable  ">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title h4" id="exampleModal{{$data->id}}">{{$data->title}}</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-sm-4">
-                                    <div class="card">
-                                        <img src="{{$data->gambar}}" alt="" max-width="100">
-                                    </div>
-                                </div>
-                                <div class="col-sm-4">
-                                    <div class="card">
-                                        <h6>{{$data->deskripsi}}</h6>
-                                    </div>
+
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <img src="{{$data->gambar}}" alt="" max-width="200">
                                 </div>
                             </div>
+                            <div class="col-sm-6">
+                                <div class="card">
+                                    <h6>{{$data->deskripsi}}</h6>
+                                </div>
+                            </div>
+                            iv>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
