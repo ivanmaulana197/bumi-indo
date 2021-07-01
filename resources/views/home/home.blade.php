bd1bfb4b1838fed2503dcde89ea8140ef05f7c93
ivanmaulana197 <ivanmaulana@webmail.umm.ac.id>
2021-07-01 09:04:31 +0700

    Update home.blade.php

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -187,22 +187,27 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <img src="{{$data->gambar}}" alt="" max-width="200">
+                        <div class="container">
+                            <div class="row">
+
+                                <div class="col-sm-6">
+                                    <img src="{{$data->gambar}}" alt="" max-width="200">
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <h6>{{$data->deskripsi}}</h6>
+                                </div>
                             </div>
-                            <div class="col-sm-6">
-                                <h6>{{$data->deskripsi}}</h6>
-                            </div>
+
+
                         </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        @endforeach
+            @endforeach
     </section><!-- End Portfolio Section -->
 
     <!-- ======= Testimonials Section ======= -->
